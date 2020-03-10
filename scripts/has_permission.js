65efce851a16a21f18166d3c1d3fd52be4072fe3
Frcsty <50297792+Frcsty@users.noreply.github.com>
2020-03-10 09:16:03 +0100

    Updated documentation

--- scripts/has_permission.js
+++ scripts/has_permission.js
@@ -1,20 +1,35 @@
-// create a variable and name it whatever you want like this
-// and use the placeholder you want, i'm using this one
+/*
+A simple placeholder which checks if the user has a permission
+
+First we create our permission variables which will give a boolean output whether or not
+the user has the specified permission
+*/
 var haspermission = "%player_has_permission_permission.test%";
 
-// create a function with the name you want
-function permission() {
+/*
+Here we create a function called hasPermission (can be named whatever you'd like) 
+which will do all the checks
+*/
+function hasPermission() {
 
-// if the haspermission variable that we created before returns yes (true boolean)
-// the js placeholder will return what we set in the return down
+  
+/*
+If the haspermission variable which we created above returns yes (true boolean)
+the placeholder will return the bellow if statements value
+*/
   if (haspermission === "yes") {
     return "&aYou have the Test permission!";
   }
 
-// if the haspermission variable wasnt true it will return what we set down
+/*
+If the haspermission variable doesn't return the above state (true boolean), 
+the user does not have the checked permission, so we return the bellow else statements value
+*/
   else {
     return "&cYou don't have the Test permission!";
   }
 }
-// by this we are calling the function to run
-permission();
+/*
+Here we call the hasPermission function so it runs
+*/
+hasPermission();
