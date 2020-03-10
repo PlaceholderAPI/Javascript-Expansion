6e62792e3b5bb15583956e5397320a9ae6cb2286
Frcsty <50297792+Frcsty@users.noreply.github.com>
2020-03-10 09:07:19 +0100

    Update factions.js
    Smaller changes

--- scripts/factions.js
+++ scripts/factions.js
@@ -1,17 +1,34 @@
-// A simple placeholder which will return brackets around the faction name if the user 
-// is in a faction, if they are not it will return nothing
+/*
+A simple placeholder which will return brackets around the faction name if the user 
+is in a faction, if they are not it will return nothing
 
-// Creating our faction variable which is the user's faction name (if the user is not in a faction it
-// will return the specified value in the factions config)
+Creating our faction variable which is the user's faction name (if the user is not in a faction it
+will return the specified value in the factions config)
+*/
 var faction = "%factionsuuid_faction_name%";
 
-// Creating our hasFaction function which will return the correct value
+/*
+Creating our hasFaction function which will return the correct value
+*/
 function hasFaction()
 {
-  // If the faction variable returns an empty string, we return an empty string back,
-  // and if the faction variable isn't empty we return colored brackets around it
-  // Checked value ? Boolean true : Boolean false
+  /* 
+  If the faction variable returns an empty string, we return an empty string back,
+  and if the faction variable isn't empty we return colored brackets around it
+  */
+  if (faction == '')
+  {
+    return '';
+  }
+  return '&8[&f' + faction + '&8]';
+  
+  /*
+  A compacter way of defining if statements
+  Checked value ? Boolean true : Boolean false
   return faction == '' ? '' : '&8[&f' + faction + '&8]';
+  */
 }
-// Calling the hasFaction function
+/*
+Calling the hasFaction function
+*/
 hasFaction();
