70cba51ee27200a0edce08d556e7577f7f8f50f4
Frcsty <50297792+Frcsty@users.noreply.github.com>
2020-03-10 09:00:41 +0100

    Create factions.js
    Placeholder: %javascript_factions%
    
    Returns brackets around the faction name if the user is in a faction

---
+++ scripts/factions.js
@@ -0,0 +1,17 @@
+// A simple placeholder which will return brackets around the faction name if the user 
+// is in a faction, if they are not it will return nothing
+
+// Creating our faction variable which is the user's faction name (if the user is not in a faction it
+// will return the specified value in the factions config)
+var faction = "%factionsuuid_faction_name%";
+
+// Creating our hasFaction function which will return the correct value
+function hasFaction()
+{
+  // If the faction variable returns an empty string, we return an empty string back,
+  // and if the faction variable isn't empty we return colored brackets around it
+  // Checked value ? Boolean true : Boolean false
+  return faction == '' ? '' : '&8[&f' + faction + '&8]';
+}
+// Calling the hasFaction function
+hasFaction();
