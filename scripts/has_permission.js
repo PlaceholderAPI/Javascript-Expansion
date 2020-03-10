1190734f8d257953087d878481065e7cdf6c1a0b
Frcsty <50297792+Frcsty@users.noreply.github.com>
2020-03-10 09:09:31 +0100

    Update has_permission.js (#12)
    Spell check <3

--- scripts/has_permission.js
+++ scripts/has_permission.js
@@ -1,17 +1,17 @@
-// create a variable and name it wantever you want like this
+// create a variable and name it whatever you want like this
 // and use the placeholder you want, i'm using this one
 var haspermission = "%player_has_permission_permission.test%";
 
 // create a function with the name you want
 function permission() {
 
-// if the haspermission variable that we created before return yes (true boolean)
+// if the haspermission variable that we created before returns yes (true boolean)
 // the js placeholder will return what we set in the return down
   if (haspermission === "yes") {
     return "&aYou have the Test permission!";
   }
 
-// if the haspermission varibale wasnt true it will return what we set down
+// if the haspermission variable wasnt true it will return what we set down
   else {
     return "&cYou don't have the Test permission!";
   }
