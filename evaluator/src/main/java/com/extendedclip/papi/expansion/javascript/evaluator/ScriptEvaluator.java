dd03300699629e31d5d39da654963e38db882049
Vaishnav Anil <vaishnavanil7th@gmail.com>
2021-06-01 05:32:53 +0530

    Added evaluator interface

---
+++ evaluator/src/main/java/com/extendedclip/papi/expansion/javascript/evaluator/ScriptEvaluator.java
@@ -0,0 +1,7 @@
+package com.extendedclip.papi.expansion.javascript.evaluator;
+
+import java.util.Map;
+
+public interface ScriptEvaluator {
+    Object execute(final Map<String, Object> additionalBindings, final String script);
+}
