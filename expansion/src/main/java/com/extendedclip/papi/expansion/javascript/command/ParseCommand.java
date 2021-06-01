a370a3c560d5cbcdc6fe9dd66a097ddf31b72432
Vaishnav Anil <vaishnavanil7th@gmail.com>
2021-06-02 01:27:45 +0530

    Modified ParseCommand to use ScriptEvaluatorFactory

--- expansion/src/main/java/com/extendedclip/papi/expansion/javascript/command/ParseCommand.java
+++ expansion/src/main/java/com/extendedclip/papi/expansion/javascript/command/ParseCommand.java
@@ -3,6 +3,7 @@ package com.extendedclip.papi.expansion.javascript.command;
 import com.extendedclip.papi.expansion.javascript.ExpansionUtils;
 import com.extendedclip.papi.expansion.javascript.JavascriptExpansion;
 import com.extendedclip.papi.expansion.javascript.JavascriptPlaceholder;
+import com.extendedclip.papi.expansion.javascript.evaluator.ScriptEvaluatorFactory;
 import org.bukkit.Bukkit;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.command.CommandSender;
@@ -14,20 +15,22 @@ import java.util.Arrays;
 public class ParseCommand extends ICommand {
 
     private final JavascriptExpansion expansion;
+    private final ScriptEvaluatorFactory evaluatorFactory;
 
-    public ParseCommand(JavascriptExpansion expansion) {
+    public ParseCommand(JavascriptExpansion expansion, final ScriptEvaluatorFactory evaluatorFactory) {
         this.expansion = expansion;
+        this.evaluatorFactory = evaluatorFactory;
     }
 
     @Override
     public void execute(CommandSender sender, String[] args) {
-        if (args.length < 2) {
+        if (args.length < 3) {
             ExpansionUtils.sendMsg(sender, "&cIncorrect usage! &f/" + command + " parse [me/player] [code]");
             return;
         }
 
-        final String script = String.join(" ", Arrays.copyOfRange(args, 1, args.length));
-        final JavascriptPlaceholder placeholder = new JavascriptPlaceholder( "parse-command", String.join(" ", script),  (a, b) -> "");
+        final String script = String.join(" ", Arrays.copyOfRange(args, 2, args.length));
+        final JavascriptPlaceholder placeholder = new JavascriptPlaceholder( "parse-command", String.join(" ", script),  evaluatorFactory);
 
         if ("me".equalsIgnoreCase(args[0])) {
             if (!(sender instanceof Player)) {
