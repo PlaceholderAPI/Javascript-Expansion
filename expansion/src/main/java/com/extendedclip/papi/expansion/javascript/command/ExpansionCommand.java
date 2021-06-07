faab2988e259dd34d1c1e257ded8560d7cf98b18
Vaishnav Anil <vaishnavanil7th@gmail.com>
2021-06-07 15:10:27 +0530

    Added tab completion option to subcommand

--- expansion/src/main/java/com/extendedclip/papi/expansion/javascript/command/ExpansionCommand.java
+++ expansion/src/main/java/com/extendedclip/papi/expansion/javascript/command/ExpansionCommand.java
@@ -3,22 +3,29 @@ package com.extendedclip.papi.expansion.javascript.command;
 import org.bukkit.command.CommandSender;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Collection;
+
 public abstract class ExpansionCommand {
 
     private final String parentCommandName;
     public final String name;
 
-    public ExpansionCommand(final String parentCommandName, final String name) {
+    public ExpansionCommand(@NotNull final String parentCommandName, @NotNull final String name) {
         this.parentCommandName = parentCommandName;
         this.name = name;
     }
 
     public abstract void execute(final CommandSender sender, final String[] args);
 
+    @NotNull
+    public abstract Collection<String> tabComplete(final CommandSender sender, final String[] args);
+
+    @NotNull
     public final String getParentCommandName() {
         return parentCommandName;
     }
 
+    @NotNull
     public final String getName() {
         return name;
     }
