9fd62e879d6f512dcb75777c79bff2b1b9a70539
Vaishnav Anil <vaishnavanil7th@gmail.com>
2021-06-07 16:38:08 +0530

    Updated debug command to adhere to new router

--- expansion/src/main/java/com/extendedclip/papi/expansion/javascript/command/impl/DebugCommand.java
+++ expansion/src/main/java/com/extendedclip/papi/expansion/javascript/command/impl/DebugCommand.java
@@ -5,17 +5,24 @@ import com.extendedclip.papi.expansion.javascript.JavascriptExpansion;
 import com.extendedclip.papi.expansion.javascript.JavascriptPlaceholder;
 import com.extendedclip.papi.expansion.javascript.command.ExpansionCommand;
 import org.bukkit.command.CommandSender;
+import org.bukkit.util.StringUtil;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import java.util.stream.Collectors;
 
-public class DebugCommand extends ExpansionCommand {
+public final class DebugCommand extends ExpansionCommand {
+    private static final String ARG_LOAD = "loaddata";
+    private static final String ARG_SAVE = "savedata";
+    private static final String NAME = "debug";
 
     private final JavascriptExpansion expansion;
 
     public DebugCommand(final String parentCommandName, final JavascriptExpansion expansion) {
-        super(parentCommandName, "debug");
+        super(parentCommandName, NAME);
         this.expansion = expansion;
     }
 
@@ -32,13 +39,31 @@ public class DebugCommand extends ExpansionCommand {
             return;
         }
 
-        if (args[0].equals("savedata")) {
+        if (args[0].equals(ARG_SAVE)) {
             jsp.saveData();
             ExpansionUtils.sendMsg(sender, "&aJavascript data '" + args[1] + "' successfully saved");
-        } else if (args[0].equals("loaddata")) {
+        } else if (args[0].equals(ARG_LOAD)) {
             jsp.loadData();
             ExpansionUtils.sendMsg(sender, "&aJavascript data '" + args[1] + "' successfully loaded");
         }
+    }
+
+    @Override
+    public @NotNull List<String> tabComplete(final CommandSender sender, final String[] args) {
+        if (args.length == 1) {
+            return StringUtil.copyPartialMatches(args[0], Arrays.asList(ARG_SAVE, ARG_LOAD), new ArrayList<>());
+        }
+        return Collections.emptyList();
+    }
+
+    @Override
+    protected @NotNull String getCommandFormat() {
+        return "debug [savedata/loaddata] [identifier]";
+    }
+
+    @Override
+    protected @NotNull String getDescription() {
+        return "Test JavaScript code in chat";
     }
 
     public String getIdentifier(String[] args) {
