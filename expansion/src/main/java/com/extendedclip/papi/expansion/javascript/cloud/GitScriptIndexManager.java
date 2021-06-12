e431ff54c2e40b2ee833e4da6fc91195b4f34d00
Vaishnav Anil <vaishnavanil7th@gmail.com>
2021-06-12 21:11:08 +0530

    Added basic index refreshing

--- expansion/src/main/java/com/extendedclip/papi/expansion/javascript/cloud/GitScriptIndexManager.java
+++ expansion/src/main/java/com/extendedclip/papi/expansion/javascript/cloud/GitScriptIndexManager.java
@@ -1,21 +1,58 @@
 package com.extendedclip.papi.expansion.javascript.cloud;
 
-import java.io.IOException;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.io.*;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public final class GitScriptIndexManager implements ScriptIndexManager {
+    private static final Gson GSON = new Gson();
     private static final String INDEX_URL =
             "https://raw.githubusercontent.com/PlaceholderAPI/" +
             "Javascript-Expansion/master/scripts/master_list.json";
 
-    @Override
-    public Optional<ScriptIndex> getScriptIndex() {
-        return Optional.empty();
+    @NotNull
+    private final JavaPlugin plugin;
+
+    private ScriptIndex index = null;
+
+    public GitScriptIndexManager(@NotNull final JavaPlugin plugin) {
+        this.plugin = plugin;
     }
 
     @Override
-    public void refreshIndex(Consumer<ScriptIndex> indexConsumer) throws IOException {
+    public synchronized Optional<ScriptIndex> getScriptIndex() {
+        return Optional.ofNullable(index);
+    }
 
+    @Override
+    public void refreshIndex(@Nullable Consumer<ScriptIndex> indexConsumer) {
+        Bukkit.getScheduler().runTaskAsynchronously(plugin, () -> {
+            try(final Reader indexReader = new InputStreamReader(new URL(INDEX_URL).openStream())) {
+                final List<GitScript> scripts = GSON.fromJson(indexReader, new TypeToken<ArrayList<GitScript>>() {}.getType());
+                final Map<String, GitScript> map = scripts.stream().collect(
+                        Collectors.toMap(GitScript::getName, Function.identity())
+                );
+                synchronized (this) {
+                    index = new GitScriptIndex(map);
+                }
+            } catch (final IOException e) {
+                e.printStackTrace();
+            }
+        });
     }
 }
