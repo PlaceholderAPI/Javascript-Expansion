dcfb25fd7e1b60f906626ad0ef98f3766676acb5
Vaishnav Anil <vaishnavanil7th@gmail.com>
2021-06-13 05:06:40 +0530

    Refactored name from ScriptIndexManager to ScriptIndexProvider

--- expansion/src/main/java/com/extendedclip/papi/expansion/javascript/cloud/ScriptIndexProvider.java
+++ expansion/src/main/java/com/extendedclip/papi/expansion/javascript/cloud/ScriptIndexProvider.java
@@ -1,12 +1,9 @@
 package com.extendedclip.papi.expansion.javascript.cloud;
 
-import com.extendedclip.papi.expansion.javascript.cloud.ScriptIndex;
-
-import java.io.IOException;
 import java.util.Optional;
 import java.util.function.Consumer;
 
-public interface ScriptIndexManager {
+public interface ScriptIndexProvider {
     Optional<ScriptIndex> getScriptIndex();
     void refreshIndex(final Consumer<ScriptIndex> indexConsumer);
 }
