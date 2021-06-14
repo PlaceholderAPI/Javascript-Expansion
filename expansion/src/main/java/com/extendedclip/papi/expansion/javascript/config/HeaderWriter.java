a2ea3ff7e60441b718836b36de50363dbfe21bf4
Vaishnav Anil <vaishnavanil7th@gmail.com>
2021-06-15 04:18:51 +0530

    Added HeaderWriter

---
+++ expansion/src/main/java/com/extendedclip/papi/expansion/javascript/config/HeaderWriter.java
@@ -0,0 +1,7 @@
+package com.extendedclip.papi.expansion.javascript.config;
+
+import org.bukkit.configuration.file.FileConfiguration;
+
+public interface HeaderWriter {
+    void writeTo(final FileConfiguration configuration);
+}
