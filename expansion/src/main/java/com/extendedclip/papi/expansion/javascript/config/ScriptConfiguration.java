9ee5dc3ab027e9de681a11c0b14d051111fff520
Vaishnav Anil <vaishnavanil7th@gmail.com>
2021-06-15 04:19:43 +0530

    Refactored File usage to Path

--- expansion/src/main/java/com/extendedclip/papi/expansion/javascript/config/ScriptConfiguration.java
+++ expansion/src/main/java/com/extendedclip/papi/expansion/javascript/config/ScriptConfiguration.java
@@ -3,19 +3,23 @@ package com.extendedclip.papi.expansion.javascript.config;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.io.File;
+import java.nio.file.Path;
 import java.util.Collection;
 import java.util.Map;
 
 public interface ScriptConfiguration {
     @Nullable
-    File getFile(@NotNull String scriptName);
+    Path getPath(@NotNull String scriptName);
 
-    void setFile(@NotNull String scriptName, @Nullable File file);
+    void setPath(@NotNull String scriptName, @Nullable final String name);
 
     @NotNull
     Collection<String> getScripts();
 
     @NotNull
-    Map<String, File> getEntries();
+    Map<String, Path> getEntries();
+
+    void reload();
+
+    void save();
 }
