aeaa76f6151fcc1aa6be2b0dc95379aa0b297306
Vaishnav Anil <vaishnavanil7th@gmail.com>
2021-06-16 03:31:31 +0530

    Readded Eval and EvalFactory as exposed classes

--- evaluator-api/src/main/java/com/extendedclip/papi/expansion/javascript/evaluator/ScriptEvaluatorFactory.java
+++ evaluator-api/src/main/java/com/extendedclip/papi/expansion/javascript/evaluator/ScriptEvaluatorFactory.java
@@ -16,7 +16,7 @@ public interface ScriptEvaluatorFactory {
         final URL selfJar = ScriptEvaluatorFactory.class
                 .getProtectionDomain()
                 .getCodeSource().getLocation();
-        final ExposingClassLoader classLoader = new ExposingClassLoader(new URL[]{selfJar});
+        final ExposingClassLoader classLoader = new ExposingClassLoader(new URL[]{selfJar}, ScriptEvaluator.class, ScriptEvaluatorFactory.class);
         final Class<?> clazz = Class.forName(IMPLEMENTATION_QUALIFIED_NAME.replace('#', '.'), true, classLoader);
         final Constructor<?> factoryConstructor = clazz.getConstructor();
         return (ScriptEvaluatorFactory) factoryConstructor.newInstance();
