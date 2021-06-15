855e39db96111dd1a1d7a0fa1761dca241adfa91
Vaishnav Anil <vaishnavanil7th@gmail.com>
2021-06-16 04:41:27 +0530

    Added default factory function

--- expansion/src/main/java/com/extendedclip/papi/expansion/javascript/config/HeaderWriter.java
+++ expansion/src/main/java/com/extendedclip/papi/expansion/javascript/config/HeaderWriter.java
@@ -2,6 +2,12 @@ package com.extendedclip.papi.expansion.javascript.config;
 
 import org.bukkit.configuration.file.FileConfiguration;
 
+import java.net.URL;
+
 public interface HeaderWriter {
     void writeTo(final FileConfiguration configuration);
+
+    static HeaderWriter fromJar(final URL jarUrl) {
+        return new ResourceHeaderWriter(new JarResourceProvider(jarUrl));
+    }
 }
