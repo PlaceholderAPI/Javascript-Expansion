1bfef30fb77821321f9685dba2ad3a464713df1d
Vaishnav Anil <vaishnavanil7th@gmail.com>
2021-06-16 04:59:45 +0530

    Refactored ReloadCommand to depend on ScriptLoader

--- expansion/src/main/java/com/extendedclip/papi/expansion/javascript/commands/ReloadCommand.java
+++ expansion/src/main/java/com/extendedclip/papi/expansion/javascript/commands/ReloadCommand.java
@@ -3,27 +3,35 @@ package com.extendedclip.papi.expansion.javascript.commands;
 import com.extendedclip.papi.expansion.javascript.ExpansionUtils;
 import com.extendedclip.papi.expansion.javascript.JavascriptExpansion;
 import com.extendedclip.papi.expansion.javascript.commands.router.ExpansionCommand;
+import com.extendedclip.papi.expansion.javascript.script.ScriptLoader;
 import org.bukkit.command.CommandSender;
 import org.jetbrains.annotations.NotNull;
 
+import java.io.IOException;
 import java.util.Collections;
 import java.util.List;
 
 public final class ReloadCommand extends ExpansionCommand {
 
-    private final JavascriptExpansion expansion;
+    private final ScriptLoader loader;
 
-    public ReloadCommand(final String parentCommandName, final JavascriptExpansion expansion) {
+    public ReloadCommand(final String parentCommandName, final ScriptLoader loader) {
         super(parentCommandName, "reload");
-        this.expansion = expansion;
+        this.loader = loader;
     }
 
     @Override
     public void execute(final CommandSender sender, final String[] args) {
 
         ExpansionUtils.sendMsg(sender, "&aJavascriptExpansion reloading...");
-        final int scripts = expansion.reloadScripts();
-        ExpansionUtils.sendMsg(sender, scripts + " &7script" + ExpansionUtils.plural(scripts) + " loaded");
+        try {
+            final int scripts = loader.reload();
+            ExpansionUtils.sendMsg(sender, scripts + " &7script" + ExpansionUtils.plural(scripts) + " loaded");
+        } catch (final IOException exception) {
+            ExpansionUtils.errorLog("&7Failed to reload scripts.", exception);
+            ExpansionUtils.sendMsg(sender, "&7Failed to reload scripts.");
+            exception.printStackTrace();
+        }
     }
 
     @Override
