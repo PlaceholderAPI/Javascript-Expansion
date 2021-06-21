3c14083b29a525fa4b623f99213af08debe8de62
Vaishnav Anil <vaishnavanil7th@gmail.com>
2021-06-22 04:02:07 +0530

    Added adapter in loading

--- evaluator/src/main/java/com/extendedclip/papi/expansion/javascript/evaluator/J2V8ScriptEvaluatorFactory.java
+++ evaluator/src/main/java/com/extendedclip/papi/expansion/javascript/evaluator/J2V8ScriptEvaluatorFactory.java
@@ -1,15 +1,86 @@
 package com.extendedclip.papi.expansion.javascript.evaluator;
 
+import io.github.slimjar.app.builder.ApplicationBuilder;
+import io.github.slimjar.app.builder.InjectingApplicationBuilder;
+import io.github.slimjar.injector.loader.Injectable;
+import io.github.slimjar.injector.loader.InstrumentationInjectable;
+import io.github.slimjar.injector.loader.UnsafeInjectable;
+import io.github.slimjar.injector.loader.WrappedInjectableClassLoader;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.*;
+import java.nio.channels.Channels;
+import java.nio.channels.FileChannel;
+import java.nio.channels.ReadableByteChannel;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Map;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
 
 public final class J2V8ScriptEvaluatorFactory implements ScriptEvaluatorFactory {
+    private static final String[] libraries = {
+        "j2v8_linux_x86_64-6.2.0.isolated-jar",
+        "j2v8_win32_x86_64-6.2.0.isolated-jar",
+        "v8-adapter-1.59.isolated-jar",
+    };
+    private static final URL SELF_JAR_URL = J2V8ScriptEvaluatorFactory.class.getProtectionDomain()
+            .getCodeSource().getLocation();
+
+    private J2V8ScriptEvaluatorFactory() {
+
+    }
+
     @Override
     public ScriptEvaluator create(final Map<String, Object> bindings) {
         return new J2V8ScriptEvaluator(bindings);
     }
 
-    @Override
-    public void cleanBinaries() {
+    public static ScriptEvaluatorFactory create() throws URISyntaxException, ReflectiveOperationException, NoSuchAlgorithmException, IOException {
+        final Collection<URL> libraryURLs = extractLibraries();
+        final ClassLoader bukkitClassLoader = J2V8ScriptEvaluatorFactory.class.getClassLoader().getParent();
+        final Injectable injectable = Injectables.createInjectable(bukkitClassLoader);
+        for (final URL libraryURL : libraryURLs) {
+            injectable.inject(libraryURL);
+        }
+        return new J2V8ScriptEvaluatorFactory();
+    }
 
+    private static Collection<URL> extractLibraries() throws IOException, URISyntaxException {
+        final Collection<URL> extracted = new ArrayList<>();
+        final File selfFile = new File(SELF_JAR_URL.toURI());
+        final JarFile jarFile = new JarFile(selfFile);
+        for (final String library : libraries) {
+            final File extractedFile = getExtractionFile(library, selfFile);
+            if (extractedFile.exists()) {
+                extracted.add(extractedFile.toURI().toURL());
+                continue;
+            }
+            final ZipEntry entry = jarFile.getEntry(library);
+            if (entry == null) {
+                continue;
+            }
+            extractedFile.getParentFile().mkdirs();
+            extractedFile.createNewFile();
+            try (final InputStream stream = jarFile.getInputStream(entry);
+                 final ReadableByteChannel inChannel = Channels.newChannel(stream);
+                 final FileChannel outChannel = FileChannel.open(extractedFile.toPath(), StandardOpenOption.CREATE, StandardOpenOption.WRITE)) {
+                outChannel.transferFrom(inChannel, 0, entry.getSize());
+            }
+            extracted.add(extractedFile.toURI().toURL());
+        }
+        return extracted;
+    }
+
+    private static File getExtractionFile(final String name, final File selfFile) {
+        return new File(selfFile.getParentFile(), "libraries/" + name.replace("isolated-jar", "jar"));
     }
 }
