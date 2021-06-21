9a8474dd5fbd63a562eae43e75197f99950bf6d3
Vaishnav Anil <vaishnavanil7th@gmail.com>
2021-06-22 04:05:44 +0530

    Removed unused create function

--- evaluator-api/src/main/java/com/extendedclip/papi/expansion/javascript/evaluator/ScriptEvaluatorFactory.java
+++ evaluator-api/src/main/java/com/extendedclip/papi/expansion/javascript/evaluator/ScriptEvaluatorFactory.java
@@ -1,18 +1,10 @@
 package com.extendedclip.papi.expansion.javascript.evaluator;
 
-import java.lang.reflect.Constructor;
-import java.net.URL;
 import java.util.Map;
 
 public interface ScriptEvaluatorFactory {
 
     ScriptEvaluator create(final Map<String, Object> bindings);
 
-    void cleanBinaries();
-
-    @SuppressWarnings("unchecked")
-    static ScriptEvaluatorFactory create() {
-
-        return new J2V8ScriptEvaluatorFactory();
-    }
+    default void cleanBinaries() {}
 }
