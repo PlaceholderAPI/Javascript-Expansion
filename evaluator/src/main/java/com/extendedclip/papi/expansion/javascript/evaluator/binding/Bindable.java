236733712c6cd06afb7682433a93411fe3fe63b8
Vaishnav Anil <vaishnavanil7th@gmail.com>
2021-06-22 00:57:17 +0530

    Removed unused Javet classes

--- evaluator/src/main/java/com/extendedclip/papi/expansion/javascript/evaluator/binding/Bindable.java
+++
@@ -1,8 +0,0 @@
-package com.extendedclip.papi.expansion.javascript.evaluator.binding;
-
-import java.lang.reflect.Method;
-
-public interface Bindable {
-    void bindMethod(final Object object, final String name, final Method method) throws BindingException;
-    void bindProperty(final Object object, final String name, final Method getter, final Method setter) throws BindingException;
-}
--- evaluator/src/main/java/com/extendedclip/papi/expansion/javascript/evaluator/binding/BindingException.java
+++
@@ -1,7 +0,0 @@
-package com.extendedclip.papi.expansion.javascript.evaluator.binding;
-
-public final class BindingException extends Exception {
-    public BindingException(Throwable cause) {
-        super("Failed to bind variable.", cause);
-    }
-}
