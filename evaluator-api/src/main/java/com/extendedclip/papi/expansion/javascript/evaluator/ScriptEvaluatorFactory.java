3eeb5f008a7ada6b15338d34048b1ccb5c63b89f
Vaishnav Anil <vaishnavanil7th@gmail.com>
2021-06-22 01:02:13 +0530

    Removed isolated loading

--- evaluator-api/src/main/java/com/extendedclip/papi/expansion/javascript/evaluator/ScriptEvaluatorFactory.java
+++ evaluator-api/src/main/java/com/extendedclip/papi/expansion/javascript/evaluator/ScriptEvaluatorFactory.java
@@ -5,20 +5,14 @@ import java.net.URL;
 import java.util.Map;
 
 public interface ScriptEvaluatorFactory {
-    String IMPLEMENTATION_QUALIFIED_NAME = "com#extendedclip#papi#expansion#javascript#evaluator#J2V8ScriptEvaluatorFactory";
 
     ScriptEvaluator create(final Map<String, Object> bindings);
 
     void cleanBinaries();
 
     @SuppressWarnings("unchecked")
-    static ScriptEvaluatorFactory isolated() throws ReflectiveOperationException {
-        final URL selfJar = ScriptEvaluatorFactory.class
-                .getProtectionDomain()
-                .getCodeSource().getLocation();
-        final ExposingClassLoader classLoader = new ExposingClassLoader(new URL[]{selfJar}, ScriptEvaluator.class, ScriptEvaluatorFactory.class);
-        final Class<?> clazz = Class.forName(IMPLEMENTATION_QUALIFIED_NAME.replace('#', '.'), true, classLoader);
-        final Constructor<?> factoryConstructor = clazz.getConstructor();
-        return (ScriptEvaluatorFactory) factoryConstructor.newInstance();
+    static ScriptEvaluatorFactory create() {
+
+        return new J2V8ScriptEvaluatorFactory();
     }
 }
