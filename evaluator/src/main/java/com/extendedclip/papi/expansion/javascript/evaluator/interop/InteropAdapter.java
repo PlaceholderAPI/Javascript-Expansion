b097b6a3c84f6dbf2ab1f7699fb3e2a2c4ce35ce
Vaishnav Anil <vaishnavanil7th@gmail.com>
2021-06-24 17:30:42 +0530

    Added simple adapters

---
+++ evaluator/src/main/java/com/extendedclip/papi/expansion/javascript/evaluator/interop/InteropAdapter.java
@@ -0,0 +1,5 @@
+package com.extendedclip.papi.expansion.javascript.evaluator.interop;
+
+public interface InteropAdapter {
+    void injectAdapter(final Object object);
+}
---
+++ evaluator/src/main/java/com/extendedclip/papi/expansion/javascript/evaluator/interop/LazyInteropAdapter.java
@@ -0,0 +1,4 @@
+package com.extendedclip.papi.expansion.javascript.evaluator.interop;
+
+public final class LazyInteropAdapter {
+}
