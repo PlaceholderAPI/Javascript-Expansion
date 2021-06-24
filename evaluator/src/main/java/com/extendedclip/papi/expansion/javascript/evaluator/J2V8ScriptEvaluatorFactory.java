a4f5c18dc5799e61f3a27e6ca9858e5ceba86f64
Vaishnav Anil <vaishnavanil7th@gmail.com>
2021-06-24 17:34:47 +0530

    Added reflectively fetching cacheMap

--- evaluator/src/main/java/com/extendedclip/papi/expansion/javascript/evaluator/J2V8ScriptEvaluatorFactory.java
+++ evaluator/src/main/java/com/extendedclip/papi/expansion/javascript/evaluator/J2V8ScriptEvaluatorFactory.java
@@ -1,10 +1,13 @@
 package com.extendedclip.papi.expansion.javascript.evaluator;
 
+import com.eclipsesource.v8.V8;
+import io.alicorn.v8.V8JavaAdapter;
 import io.github.slimjar.injector.loader.Injectable;
 
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.lang.reflect.Field;
 import java.net.*;
 import java.nio.channels.Channels;
 import java.nio.channels.FileChannel;
@@ -23,9 +26,13 @@ public final class J2V8ScriptEvaluatorFactory implements ScriptEvaluatorFactory 
     };
     private static final URL SELF_JAR_URL = J2V8ScriptEvaluatorFactory.class.getProtectionDomain()
             .getCodeSource().getLocation();
+    private final Map<V8, ?> runtimeToCacheMap;
 
-    private J2V8ScriptEvaluatorFactory() {
 
+    private J2V8ScriptEvaluatorFactory() throws NoSuchFieldException, IllegalAccessException {
+        final Field cacheMap = V8JavaAdapter.class.getDeclaredField("runtimeToCacheMap");
+        cacheMap.setAccessible(true);
+        runtimeToCacheMap = (Map<V8, ?>) cacheMap.get(null);
     }
 
     @Override
