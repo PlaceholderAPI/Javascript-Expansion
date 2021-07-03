b8db9d16adce5102632fa031b151017701c04e7e
Vaishnav Anil <vaishnavanil7th@gmail.com>
2021-07-03 20:55:34 +0530

    Removed Injection functions from QuickJsScriptEvaluatorFactory

--- evaluator/src/main/java/com/extendedclip/papi/expansion/javascript/evaluator/QuickJsScriptEvaluatorFactory.java
+++ evaluator/src/main/java/com/extendedclip/papi/expansion/javascript/evaluator/QuickJsScriptEvaluatorFactory.java
@@ -1,5 +1,6 @@
 package com.extendedclip.papi.expansion.javascript.evaluator;
 
+import com.extendedclip.papi.expansion.javascript.evaluator.util.InjectionUtil;
 import io.github.slimjar.injector.loader.Injectable;
 import io.github.slimjar.injector.loader.InjectableFactory;
 
@@ -17,9 +18,7 @@ import java.util.jar.JarFile;
 import java.util.zip.ZipEntry;
 
 public final class QuickJsScriptEvaluatorFactory implements ScriptEvaluatorFactory {
-    private static final String[] LIBRARIES = {
-        "quickjs-1.0.0.isolated-jar",
-    };
+    private static final Collection<String> LIBRARIES = Collections.singletonList("quickjs-1.0.0.isolated-jar");
     private static final URL SELF_JAR_URL = QuickJsScriptEvaluatorFactory.class.getProtectionDomain()
             .getCodeSource().getLocation();
 
@@ -33,46 +32,7 @@ public final class QuickJsScriptEvaluatorFactory implements ScriptEvaluatorFacto
     }
 
     public static ScriptEvaluatorFactory create() throws URISyntaxException, ReflectiveOperationException, NoSuchAlgorithmException, IOException {
-        final Collection<URL> libraryURLs = extractLibraries();
-        final ClassLoader bukkitClassLoader = QuickJsScriptEvaluatorFactory.class.getClassLoader().getParent();
-        try {
-            final Injectable injectable = InjectableFactory.create(bukkitClassLoader);
-            for (final URL libraryURL : libraryURLs) {
-                injectable.inject(libraryURL);
-            }
-        } catch (final Exception exception) {
-            throw new LibraryInjectionException(exception);
-        }
+        InjectionUtil.inject(LIBRARIES);
         return new QuickJsScriptEvaluatorFactory();
-    }
-
-    private static Collection<URL> extractLibraries() throws IOException, URISyntaxException, NoSuchAlgorithmException, ReflectiveOperationException {
-        final Collection<URL> extracted = new ArrayList<>();
-        final File selfFile = new File(SELF_JAR_URL.toURI());
-        final JarFile jarFile = new JarFile(selfFile);
-        for (final String library : LIBRARIES) {
-            final File extractedFile = getExtractionFile(library, selfFile);
-            if (extractedFile.exists()) {
-                extracted.add(extractedFile.toURI().toURL());
-                continue;
-            }
-            final ZipEntry entry = jarFile.getEntry(library);
-            if (entry == null) {
-                continue;
-            }
-            extractedFile.getParentFile().mkdirs();
-            extractedFile.createNewFile();
-            try (final InputStream stream = jarFile.getInputStream(entry);
-                 final ReadableByteChannel inChannel = Channels.newChannel(stream);
-                 final FileChannel outChannel = FileChannel.open(extractedFile.toPath(), StandardOpenOption.CREATE, StandardOpenOption.WRITE)) {
-                outChannel.transferFrom(inChannel, 0, entry.getSize());
-            }
-            extracted.add(extractedFile.toURI().toURL());
-        }
-        return extracted;
-    }
-
-    private static File getExtractionFile(final String name, final File selfFile) {
-        return new File(selfFile.getParentFile(), "libraries/" + name.replace("isolated-jar", "jar"));
     }
 }
