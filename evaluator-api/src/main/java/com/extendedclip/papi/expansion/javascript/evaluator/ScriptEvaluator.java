134f0256aaed05906577ffa6431f3699ee768ed2
Vaishnav Anil <vaishnavanil7th@gmail.com>
2021-07-03 20:57:11 +0530

    Added ScriptException to throws list of execute

--- evaluator-api/src/main/java/com/extendedclip/papi/expansion/javascript/evaluator/ScriptEvaluator.java
+++ evaluator-api/src/main/java/com/extendedclip/papi/expansion/javascript/evaluator/ScriptEvaluator.java
@@ -1,7 +1,8 @@
 package com.extendedclip.papi.expansion.javascript.evaluator;
 
+import javax.script.ScriptException;
 import java.util.Map;
 
 public interface ScriptEvaluator {
-    Object execute(final Map<String, Object> additionalBindings, final String script) throws EvaluatorException;
+    Object execute(final Map<String, Object> additionalBindings, final String script) throws EvaluatorException, ScriptException;
 }
