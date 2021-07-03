2733a38b50907b1cec635b015688fffd0cabf5f1
Vaishnav Anil <vaishnavanil7th@gmail.com>
2021-07-03 22:06:51 +0530

    Added dependencies to libraries collection in nashorn

--- evaluator/src/main/java/com/extendedclip/papi/expansion/javascript/evaluator/NashornScriptEvaluatorFactory.java
+++ evaluator/src/main/java/com/extendedclip/papi/expansion/javascript/evaluator/NashornScriptEvaluatorFactory.java
@@ -6,12 +6,17 @@ import org.openjdk.nashorn.api.scripting.NashornScriptEngineFactory;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.Map;
 
 public final class NashornScriptEvaluatorFactory implements ScriptEvaluatorFactory {
-    public static final Collection<String> LIBRARIES = Collections.singletonList("nashorn-core-15.1.isolated-jar");
+    public static final Collection<String> LIBRARIES = Arrays.asList(
+            "nashorn-core-15.1.isolated-jar",
+            "asm-commons-9.2.jar",
+            "asm-util-9.2.jar",
+            "asm-9.2.jar"
+    );
     private final NashornScriptEngineFactory engineFactory;
 
     private NashornScriptEvaluatorFactory(final NashornScriptEngineFactory engineFactory) {
